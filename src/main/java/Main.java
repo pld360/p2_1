2cb623acd9a0e40b59087741e5f0d135c048ab55
pld360 <134562508+pld360@users.noreply.github.com>
2025-03-19 13:24:10 +0000

    gotowe zad z uzyciem Scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Scanner;  
 // import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("HELLO WORLD!");
+    String tekst = "hello world!";
+    System.out.println(tekst.toUpperCase());
   }
 
   // @Test
